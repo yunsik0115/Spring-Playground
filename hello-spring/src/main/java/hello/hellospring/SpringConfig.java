a1bb68ae764d31f833350d80a495cf5c92d99044
Yunsik <pubyunsik0115@gmail.com>
2021-03-28 16:08:02 +0900

    JPA....????? Cannot Resolve 에러
    추후 헤결해봐야할듯 구조를 다시 보자.....!!

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,4 +1,21 @@
 package hello.hellospring;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+}
+
+/*package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
@@ -28,7 +45,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         return new JdbcTemplateMemberRepository(dataSource);
     }
-}
+}*/
 
 
 
--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,11 @@
 package hello.hellospring.domain;
 
-public class Member {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/*public class Member {
     private Long id;
     private String name;
 
@@ -16,6 +21,27 @@ public class Member {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+}*/
+
+@Entity
+public class Member {
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
     public void setName(String name) {
         this.name = name;
     }
