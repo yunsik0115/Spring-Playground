a16bc4c8688b2f4c5e0060cbab273580cf5162e9
Yunsik <pubyunsik0115@gmail.com>
2021-03-30 15:58:11 +0900

    test

---
+++ core/src/test/java/SpringBasic/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBasic.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
