ac6fc17a6c9d38e8937e617ddf0b5de63f441d4b
Yunsik Kim <pubyunsik0115@gmail.com>
2021-04-01 21:47:13 +0900

    AppConfig Refactored (각자의 역할이 코드상에 잘 드러나도록 Refactored)

--- core/src/main/java/SpringBasic/core/AppConfig.java
+++ core/src/main/java/SpringBasic/core/AppConfig.java
@@ -1,5 +1,6 @@
 package SpringBasic.core;
 
+import SpringBasic.core.discount.DiscountPolicy;
 import SpringBasic.core.discount.FixDiscountPolicy;
 import SpringBasic.core.member.MemberService;
 import SpringBasic.core.member.MemberServiceImpl;
@@ -7,17 +8,25 @@ import SpringBasic.core.member.MemoryMemberRepository;
 import SpringBasic.core.order.OrderService;
 import SpringBasic.core.order.OrderServiceImpl;
 
-public class AppConfig {
+public class AppConfig { // 역할이 다 드러나게 Refactor
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository()); // ctrl + alt + m -> Refactoring
         //MemberServiceImpl을 만들고 MemoryMemberRepository를 사용함.
+    }
+
+    private MemoryMemberRepository memberRepository() { // DB 변경시 이 코드만 바꾸면 됨.
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
