eb2f3148244a2c05c78e055f682b49698e8d268e
Yunsik Kim <pubyunsik0115@gmail.com>
2021-04-03 21:47:33 +0900

    특정 타입을 모두 조회하는 방법 같은 타입의 Bean이 2개 이상일때 Map 이용하는 방법 추가) @Autowired할때 응용되는 경우가 있음!!

--- core/src/test/java/SpringBasic/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ core/src/test/java/SpringBasic/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -1,20 +1,47 @@
 package SpringBasic.core.beanfind;
 
 import SpringBasic.core.AppConfig;
+import SpringBasic.core.member.Member;
 import SpringBasic.core.member.MemberRepository;
 import SpringBasic.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ApplicationContextSameBeanFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
     @Test
     @DisplayName("타입으로 조회시 같은 타입 2개 이상 있을 경우, 중복 오류 발생")
     void findBeanByTypeDuplicate() {
-        MemberRepository bean = ac.getBean(MemberRepository.class); // Type만 지정 -> 예외 터짐(No Unique Bean)
+        // MemberRepository bean = ac.getBean(MemberRepository.class); // Type만 지정 -> 예외 터짐(No Unique Bean)
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class));
+    }
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 2개 이상 -> 빈 이름을 지정하면 TEST 성공")
+    void findBeanByName() {
+        MemberRepository memberRepository = ac.getBean("memberRepository1",MemberRepository.class);
+        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeanByType(){
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = "+ key + " Value = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+
     }
 
     // 클래스 안의 설정파일 클래스 -> 그 클래스 내부에서만 쓰겠다를 의미.
@@ -24,6 +51,7 @@ public class ApplicationContextSameBeanFindTest {
         public MemberRepository memberRepository1() {
             return new MemoryMemberRepository();
         }
+
         @Bean
         public MemberRepository memberRepository2() {
             return new MemoryMemberRepository();
