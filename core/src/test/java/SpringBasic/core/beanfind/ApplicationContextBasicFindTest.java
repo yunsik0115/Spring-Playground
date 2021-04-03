7a74e8830b0af283eec509fa22878533c2bcb86a
Yunsik Kim <pubyunsik0115@gmail.com>
2021-04-03 21:20:09 +0900

    Spring Bean 조회 Via Test Code

---
+++ core/src/test/java/SpringBasic/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,45 @@
+package SpringBasic.core.beanfind;
+
+import SpringBasic.core.AppConfig;
+import SpringBasic.core.member.MemberService;
+import SpringBasic.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("빈 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        //MemberService memberService = ac.getBean("xxxx", MemberService.class);
+        //MemberService xxxxx = ac.getBean("xxxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class)); // 무조건 예외가 터져야 테스트 성공이라는 의미
+    }
+}
+
+//Type와 이름으로 조회하는 경우 각각이 장단점이 존재한다.
+
