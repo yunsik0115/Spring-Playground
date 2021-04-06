451f842ae3345842f2c8ec803a8104878dbb4071
Yunsik Kim <pubyunsik0115@gmail.com>
2021-04-06 20:52:36 +0900

    Spring Bean Component 식 자동인식 VS 수동인식의 경우 수동 인식이 우선권을 가짐(자동 인식된 Bean Override) 그러나 Spring Boot부터는 에러로 표시함 (우선을 가리기 애매한 상황은 만들지 말자)

--- core/src/main/java/SpringBasic/core/AutoAppConfig.java
+++ core/src/main/java/SpringBasic/core/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package SpringBasic.core;
 
 
+import SpringBasic.core.member.MemberRepository;
+import SpringBasic.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,10 @@ import org.springframework.context.annotation.FilterType;
         // 기존에 등록한 AppConfig(TestConfig... etc)를 예외처리하기 위함.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository") // 수동 등록 빈이 자동빈을 overriding함
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository(); //MemberServiceImpl에서 Component와 중복
+    }
 }
+// 애매한 상황은 만들지 말자
+// Spring Boot 부터는 바뀌었다. (자동 빈과 수동 빈 중복시 에러)
