9c7a0bbb7b11a4001e1c296c861aac562cfd24cd
Yunsik Kim <pubyunsik0115@gmail.com>
2021-04-11 16:54:02 +0900

    Entity -> 최대한 순수하게 유지 화면 로직은 엔티티상에 없어야 함. Form 사용 권장. MemberList.html implemented

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,9 +30,16 @@ public class MemberController {
         }
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
         Member member = new Member();
+        member.setName(form.getName());
         member.setAddress(address);
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        model.addAttribute("members", memberService.findMembers());
+        return "members/memberList";
     }
 }
