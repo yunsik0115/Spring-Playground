8f6e10f05f73318c1a9326c9e5436ca99d151aeb
Yunsik Kim <pubyunsik0115@gmail.com>
2021-04-13 00:24:43 +0900

    servlet http message parsing

---
+++ servlet/src/main/java/springmvc/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,84 @@
+package springmvc.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse
+            response)
+            throws ServletException, IOException {
+        printStartLine(request);
+        printHeaders(request);
+        printHeaderUtils(request);
+        response.getWriter().write("ok");
+    }
+
+    private void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("request.getMethod() = " + request.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + request.getProtocol()); //HTTP/1.1
+        System.out.println("request.getScheme() = " + request.getScheme()); //http
+        // http://localhost:8080/request-header
+        //request-test
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        System.out.println("request.getQueryString() = " + request.getQueryString());//username=hi
+        System.out.println("request.isSecure() = " + request.isSecure()); //https 사용 유무
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+    // 헤더 정보
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers - start ---");
+/* 헤더 정보 뽑는 방법 . 구식
+Enumeration<String> headerNames = request.getHeaderNames();
+while (headerNames.hasMoreElements()) { -> 다음 요소가 있는가?
+String headerName = headerNames.nextElement(); -> 값을 꺼낸다
+System.out.println(headerName + ": " + request.getHeader(headerName)); -> 값을 출력할 수 있다.
+}
+*/
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> System.out.println(headerName + ": " + request.getHeader(headerName)));
+        System.out.println("--- Headers - end ---");
+        System.out.println();
+    }
+
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("--- Header 편의 조회 start ---");
+        System.out.println("[Host 편의 조회]");
+        System.out.println("request.getServerName() = " +
+                request.getServerName()); //Host 헤더
+        System.out.println("request.getServerPort() = " +
+                request.getServerPort()); //Host 헤더
+        System.out.println();
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " +
+                        locale));
+        System.out.println("request.getLocale() = " + request.getLocale());
+        System.out.println();
+        System.out.println("[cookie 편의 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+        System.out.println();
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " +
+                request.getContentType());
+        System.out.println("request.getContentLength() = " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " + request.getCharacterEncoding());
+        System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
+    }
+
+}
