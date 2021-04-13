ee1f5ebb080ab77ec1ff782313efe3aa8892b001
Yunsik Kim <pubyunsik0115@gmail.com>
2021-04-13 13:44:57 +0900

    comment added

--- servlet/src/main/java/springmvc/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/springmvc/servlet/basic/request/RequestParamServlet.java
@@ -18,14 +18,14 @@ public class RequestParamServlet extends HttpServlet {
                 .forEachRemaining(paramName -> System.out.println("paramName = " + req.getParameter(paramName)));
         
         // 2. 단일 parameter 조회
-        String username = req.getParameter("username");
+        String username = req.getParameter("username"); // 단일일 경우 사용
         System.out.println("username = " + username);
         String age = req.getParameter("age");
         System.out.println("age = " + age);
 
         // 3. 중복 parameter 조회
         // for example : uri/index?username=hello&age=20&username=hello2 .... etc
-        String[] usernames = req.getParameterValues("username");
+        String[] usernames = req.getParameterValues("username"); // 중복 파라미터일 경우 사용
         for (String name : usernames) {
             System.out.println("name = " + name);
         }
