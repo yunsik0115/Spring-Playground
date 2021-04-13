1b2b74f159041f2048aed47303b2407902f2fa30
Yunsik Kim <pubyunsik0115@gmail.com>
2021-04-13 13:44:15 +0900

    http request, get query data parsing

---
+++ servlet/src/main/java/springmvc/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,34 @@
+package springmvc.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 전체 parameter 조회
+        Enumeration<String> parameterNames = req.getParameterNames();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println("paramName = " + req.getParameter(paramName)));
+        
+        // 2. 단일 parameter 조회
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+        String age = req.getParameter("age");
+        System.out.println("age = " + age);
+
+        // 3. 중복 parameter 조회
+        // for example : uri/index?username=hello&age=20&username=hello2 .... etc
+        String[] usernames = req.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("name = " + name);
+        }
+
+    }
+}
